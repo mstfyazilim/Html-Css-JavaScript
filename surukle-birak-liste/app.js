bb94b15e07230e5bc1ce58c9c3fc739ac3c79d5b
mstfyazilim <157624518+mstfyazilim@users.noreply.github.com>
2024-04-24 13:56:42 +0300

    Sürükle Bırak Liste
    Javascript kütüphanesi Sortable Js ile üretilen sürükleme biçiminin yerini değiştirmesi sağlanır.

---
+++ surukle-birak-liste/app.js
@@ -0,0 +1,10 @@
+var  list=document.querySelector(".container");
+/*new Sortable(list, {
+      animation: 150,
+      ghostClass: 'blue-background-class'
+  });
+
+*/
+new Sortable(list,{
+animation:150, 
+});
